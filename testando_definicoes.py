e355da51c8d6afc741f72e37715165d5109774ff
João V. A. Carvalho <jv.carvalho.cv@gmail.com>
2025-05-26 15:26:58 -0400

    Update testando_definicoes.py

--- testando_definicoes.py
+++ testando_definicoes.py
@@ -1,18 +1,36 @@
-compras = ["pao", "café", "leite"]
-print(compras)
+# compras = ["pao", "café", "leite"]
+# print(compras)
 
-# pode ser removido pelo nome do produto ou pelo indice
-# compras.remove("café")
-compras.remove(compras[1])
-print(compras)
+# # pode ser removido pelo nome do produto ou pelo indice
+# # compras.remove("café")
+# compras.remove(compras[1])
+# print(compras)
 
-# Append acrescenta um item no final da lista, só pode adicionar um por vez
-compras.append("açicar")
-print(compras)
+# # Append acrescenta um item no final da lista, só pode adicionar um por vez
+# compras.append("açicar")
+# print(compras)
 
-# É preciso criar um alista nova para receber os elementos ordenados da lista antiga
-compras_ordenadas = sorted(compras)
-print(compras_ordenadas)
+# # É preciso criar um alista nova para receber os elementos ordenados da lista antiga
+# compras_ordenadas = sorted(compras)
+# print(compras_ordenadas)
 
-for item in compras:
-    print("-",item)
+# # O nome "panela" é um identificador os itens da lista, esses itens podem receber qualquer tpo de nome
+# for panela in compras:
+#     print("-",panela)
+
+# teste = [1,2,3,5]
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+#uma tupla
+# teste = (1,2,3,5)
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+#conjunto
+teste = {1,2,3,5}
+print(teste)
+#teste.remove(1)
+print(teste)
